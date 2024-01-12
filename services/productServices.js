e7c3139180954bd148866405e9ebc590963e540c
codethelabs <codethelabs@gmail.com>
2024-01-12 11:55:53 +0300

    adding mocha

--- services/productServices.js
+++ services/productServices.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/users'); // Update with your actual model path
+
 
 class MasterService {
     constructor() {
@@ -36,6 +39,28 @@ class MasterService {
             }
         }
     
+      
+async authenticateToken (req, res, next){
+  const token = req.header('Authorization');
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Unauthorized: No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'your-secret-key'); // Replace with your actual secret key
+    const user = await User.findOne({ _id: decoded.userId });
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'Unauthorized: Invalid token' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ success: false, message: 'Unauthorized: Invalid token' });
+  }
+};
     
     
     
